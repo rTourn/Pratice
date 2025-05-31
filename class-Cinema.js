54ae7815f15cb7c8f32dc8d1da2b042d7e41436d
rTourn <100523425+rTourn@users.noreply.github.com>
2025-05-31 19:29:21 +0200

    31/05

--- class-Cinema.js
+++ class-Cinema.js
@@ -1,3 +1,17 @@
+let previouslyId = []
+
+function generateId(){
+    let data = Date.now().toString(36) + Math.random().toString(36).substring(2,5);
+
+    while(previouslyId.includes(data)){
+        data = Date.now().toString(36) + Math.random().toString(36).substring(2,5);
+    }
+
+    previouslyId.push(data)
+    return data;
+}
+
+
 class Cinema{
     constructor(name){
         this.name = name;
@@ -8,7 +22,7 @@ class Cinema{
     addMovie(movie){
         const index = this.movies.indexOf(movie);
 
-        if(index > -1){
+        if(index === -1){
             this.movies.push(movie)
             console.log(`You added this movie: ${movie.title} to the cinema : ${this.name}`)
         }else{
@@ -19,7 +33,7 @@ class Cinema{
     removeMovie(movie){
         const index = this.movies.indexOf(movie);
 
-        if(index === -1){
+        if(index > -1){
             this.movies.splice(index, 1)
             console.log(`You have removed this movie: ${movie.title} from the stock of this cinema : ${this.name}`)
         }else{
@@ -30,7 +44,7 @@ class Cinema{
     addCustomer(member){
         const index = this.customers.indexOf(member);
 
-        if(index > -1){
+        if(index === -1){
             this.customers.push(member)
             console.log(`You added this member: ${member.name} to the cinema : ${this.name}`)
         }else{
@@ -41,7 +55,7 @@ class Cinema{
     removeCustomer(member){
         const index = this.customers.indexOf(member);
 
-        if(index === -1){
+        if(index > -1){
             this.customers.splice(index, 1)
             console.log(`You have removed this member: ${member.name} from this cinema : ${this.name}`)
         }else{
@@ -49,19 +63,35 @@ class Cinema{
         }
     }
 
-    registeredCustomer(customer){
-        const whatchList = Array.from(customer.reservations.entries()).map(([movies, times])=> ({
-                Movie: movies.title,
-                ShowTime : times
+    registeredCustomer(){
+        const customerList = this.customers.map(customer=> ({
+                Name: customer.name,
+                ID: customer.id,
+                Reservations: customer.getMyReservation()
             }))
-        console.log(`ID: ${customer.id}, Name: ${customer.name}`)
-        console.log(`Wachted`)
-        console.table(whatchList)
+        console.log(`Registred Customer`)
+        console.table(customerList)
 
     }
 
     listMovies(){
-        console.log(`Movie list : ${this.movies}`)
+        const movieList = this.movies.map(movie=> ({
+            Title: movie.title,
+            Duration: movie.duration,
+            ID: movie.id
+        }))
+
+        console.table(movieList)
+
+    }
+
+    listCustomers(){
+        const customersList = this.customers.map(customer=> ({
+            Name: customer.name,
+            ID: customer.id,
+        }))
+
+        console.table(customersList)
 
     }
 
@@ -78,14 +108,25 @@ class Cinema{
     showReservations(movie){
        movie.listReservations()
     }
+
+    hasMovie(movie){
+        const index = this.movies.indexOf(movie)
+
+        if(index === -1){
+            return false
+        }
+        return true;
+    }
 }
 
 class Movie{
-    constructor(title, duration){
+    constructor(title, duration, maxCapacity){
         this.id = generateId()
         this.title = title;
         this.duration = duration;
         this.showtimes = [];
+        this.maxCapacity = maxCapacity;
+        this.reservedPlace= 0;
         this.reservations = new Map();
     }
 
@@ -93,36 +134,49 @@ class Movie{
         this.showtimes.push(time);
     }
 
+    listShowTimes(){
+        console.log(this.showtimes)
+    }
+
     reserveSeat(customer, time){
+
+        if(this.reservedPlace > this.maxCapacity){
+            console.log(`This movie: ${this.title} is already all booked`);
+            return false;
+        }
+
         if (!this.showtimes.includes(time)) {
             console.log(`This time: ${time} is not available for this movie: ${this.title}`);
-            return;
+            return false;
         }
 
         if (this.reservations.has(customer.id)) {
             console.log(`${customer.name} has already reserved a seat.`);
-            return;
+            return false;
         }
 
         this.reservations.set(customer.id, { customer, time });
+        this.reservedPlace +=1;
         console.log(`${customer.name} reserved a seat for "${this.title}" at ${time}`);
-            
+        return true;
     }
 
     cancelReservation(customer){
 
         if(this.reservations.has(customer.id)){
              this.reservations.delete(customer.id)
+             this.reservedPlace -=1;
              console.log(`${customer.name}'s reservation has been cancelled.`)
+            return true;
         }else{
             console.log(`No reservation found for ${customer.name}`)
+            return false;
         }
-        
     }
 
     listReservations(){
         const reservationList = Array.from(this.reservations.entries()).map(([id, info])=> ({
-                id: id,
+                Id: id,
                 Name: info.customer.name,
                 showtime: info.time
             }))
@@ -142,25 +196,48 @@ class Customer{
     }
 
     makeReservation(movie, time){
-
+        if(movie.reserveSeat(this, time)){
+             this.reservations.set(movie.id, { movie, time });
+             console.table(this.getMyReservation())
+        }
     }
 
-    cancelReservation(movie, time){
-        if(!this.reservations.has(movie.id)){
-            console.log(`You don't have any reversation for ${movie.title}`)
-            return
+    cancelReservation(movie, cinema){
+
+        if(!cinema.hasMovie(movie)){
+            console.log(`Movie not found in this cinema.`)
+            return 
         }
 
-        const info = this.reservations.get()
-
-        if(!this.reservations.has(time)){
-            
+        if(movie.cancelReservation(this)){
+            this.reservations.delete(movie.id);
+            console.table(this.getMyReservation())
         }
+    }
 
+    hasReservation(movie){
+        if(this.reservations.has(movie.id)){
+            return true;
+        }
+        return false;
     }
 
     listMyReservations(){
+        const reservationList = Array.from(this.reservations.entries()).map(([id, info])=> ({
+            Id: id,
+            Name: info.movie.title,
+            showtime: info.time
+        }))
+        console.log(`Reservations for ${this.name}`)
+        console.table(reservationList)
 
     }
 
+    getMyReservation(){
+        return Array.from(this.reservations.entries()).map(([id, info])=> ({
+            Id: id,
+            Name: info.movie.title,
+            showtime: info.time
+        }))
+    }
 }
